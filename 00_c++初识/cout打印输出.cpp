d8a3098845a97ada0bc849ffb191a3faccdc80e1
cfl <1696741038@qq.com>
2024-06-14 15:10:08 +0800

    init

---
+++ 00_c++初识/cout打印输出.cpp
@@ -0,0 +1,19 @@
+//
+// Created by chenf on 2024/6/14.
+//
+#include <iostream>
+using namespace std;
+
+/*
+ *
+ */
+int main()
+{
+    cout << "hello" << " world" << " 1 " << endl;
+    cout << 10 << endl;
+
+    cout << "I am 10 " << "years old." << endl;
+    cout << "C++ is" << " the best" << " programming language." << endl;
+    cout << "If you want to learn It," << "come to itheima." << endl;
+    return 0;
+}
---
+++ 01_第一章——数据类型、运算符与表达式/字面常量.cpp
@@ -0,0 +1,12 @@
+//
+// Created by chenf on 2024/6/14.
+//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // 整形、实形、字符、字符串
+
+    return 0;
+}
